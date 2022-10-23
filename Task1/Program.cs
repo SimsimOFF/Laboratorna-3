b97ca99c1e8ca5ff3f1fb85f65e761951ec11ff9
{{SimsimOFF}} <{{S.Melik-Kazaryan_FIT_11_21_B_d@knute.edu.ua}}>
2022-10-23 22:56:53 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Я вивчаю цю мову не так давно, тому не дуже зрозумів як зробити це завдання.
+        }
+    }
+}
